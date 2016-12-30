3053ac165bff1a56b95cd482f1d80b2465c36760
bastianbuhrkall <bastian_buhrkall@hotmail.com>
2016-12-30 23:30:29 +0100

    Git Commit

--- src/getcreditscore/GetCreditScore.java
+++ src/getcreditscore/GetCreditScore.java
@@ -5,9 +5,13 @@
  */
 package getcreditscore;
 
+import com.rabbitmq.client.AMQP;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Consumer;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
 import java.io.IOException;
 import java.util.concurrent.TimeoutException;
 
@@ -20,31 +24,44 @@ public class GetCreditScore {
     /**
      * @param args the command line arguments
      */
-    
-    private final static String SENDING_QUEUE_NAME = "2";
+    private final static String SENDING_QUEUE_NAME = "RuleBaseQueue";
+    private final static String LISTENING_QUEUE_NAME = "CreditScoreQueue";
 
-    
+    static String message = "";
+
     public static void main(String[] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("datdb.cphbusiness.dk");
+        factory.setVirtualHost("student");
+        factory.setUsername("Dreamteam");
+        factory.setPassword("bastian");
+
+        Connection connection = factory.newConnection();
+        Channel sendingChannel = connection.createChannel();
+        Channel listeningChannel = connection.createChannel();
+
+        listeningChannel.queueDeclare(LISTENING_QUEUE_NAME, false, false, false, null);
         
-            ConnectionFactory factory = new ConnectionFactory();
-            factory.setHost("localhost");
-            factory.setHost("datdb.cphbusiness.dk");
-            factory.setVirtualHost("student");
-            factory.setUsername("student");
-            factory.setPassword("cph");
-            
-            Connection connection = factory.newConnection();
-            Channel channel = connection.createChannel();
-    
-            channel.queueDeclare(SENDING_QUEUE_NAME, false, false, false, null);
-            String message = "" + creditScore("222222-5555");
-            channel.basicPublish("", SENDING_QUEUE_NAME, null, message.getBytes());
- 
-            System.out.println(" [x] Sent '" + message + "'");
-    
-            channel.close();
-            connection.close();
-         
+        Consumer consumer = new DefaultConsumer(listeningChannel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
+                    throws IOException {
+                message = new String(body, "UTF-8");
+                System.out.println(" [x] Received '" + message + "'");
+
+                String[] arr = message.split(",");
+                
+                sendingChannel.queueDeclare(SENDING_QUEUE_NAME, false, false, false, null);
+
+                String message = arr[0] + "," + creditScore(arr[0]) + "," + arr[1] + "," + arr[2] ;
+                
+                sendingChannel.basicPublish("", SENDING_QUEUE_NAME, null, message.getBytes());
+                System.out.println(" [x] Sent '" + message + "'");
+
+            }
+        };
+        
+        listeningChannel.basicConsume(LISTENING_QUEUE_NAME, true, consumer);
     }
 
     private static int creditScore(java.lang.String ssn) {
@@ -52,8 +69,5 @@ public class GetCreditScore {
         org.bank.credit.web.service.CreditScoreService port = service.getCreditScoreServicePort();
         return port.creditScore(ssn);
     }
-    
-    
-    
-    
+
 }
